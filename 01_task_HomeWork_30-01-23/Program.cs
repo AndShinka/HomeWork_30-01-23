68081ae34f8f069ede39a89dc04a6d5e46f1b775
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-02-02 09:47:04 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ 01_task_HomeWork_30-01-23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Clear();
+Console.WriteLine();
+
+int numDay = 0;
+string[] WeekDay = {"нулевой", "понедельник", "вторник", "среда", "четверг", "пятница",
+                     "Суббота", "Воскресенье"};
+//metka:
+do
+{
+   Console.WriteLine("Введите число в диапазоне от 1 до 7, обозначающее номер дня недели : "); 
+   numDay = Convert.ToInt32(Console.ReadLine());
+}
+while ((numDay < 1) | (numDay >7));
+//Console.WriteLine("Введено число: " + numDay);
+
+if (numDay >5) 
+{
+    Console.WriteLine("Ура!!! День недели под номером " + numDay + " - это " 
+                        + WeekDay[numDay] +", Выходной день !");
+}
+else
+{
+    Console.WriteLine("День недели под номером " + numDay + " - это " 
+                        + WeekDay[numDay] +", рабочий день.");
+}
+Console.WriteLine();
+Console.WriteLine();
+
+//goto metka;
